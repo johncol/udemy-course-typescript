6e41be9f07f7419d4e5d984b969829a0c2e12ed6
John Cely <jcely@twilio.com>
2019-10-21 19:10:22 -0500

    Add section 1 sample code
    Use axios to send an http request and log the response

---
+++ section-01-getting-started/README.md
@@ -0,0 +1,26 @@
+# Getting Started
+
+Course https://www.udemy.com/course/typescript-the-complete-developers-guide
+
+## Environment
+
+Install typescript and ts-node on your machine
+
+```
+npm install -g typescript ts-node
+```
+
+## Run your code
+
+1. Use typescript compiler, then node to run the code
+
+```
+tsc index.ts // index.js is generated
+node index.js
+```
+
+2. Use ts-node
+
+```
+ts-node index.ts
+```
---
+++ section-01-getting-started/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url: string = 'https://jsonplaceholder.typicode.com/todos/1';
+
+interface ToDo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const logTodo = (todo: ToDo) => {
+  const { id, title, completed } = todo;
+  console.log(`
+    TODO with ID ${id}
+    Has title of "${title}"
+    Is it finished? ${completed}
+  `);
+};
+
+axios.get(url).then(response => {
+  const todo: ToDo = response.data;
+  logTodo(todo);
+});
